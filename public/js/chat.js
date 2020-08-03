6887b84ea1717200d156189d99dd0205f27b9d3f
vanessavps <vanessa.vps29@gmail.com>
2020-08-03 18:45:12 +1200

    Hide user list on mobile

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,7 @@
 const socket = io()
 
 const $messageForm = document.querySelector('#message-form')
-const $messageFormInput = $messageForm.querySelector('textarea')
+const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
